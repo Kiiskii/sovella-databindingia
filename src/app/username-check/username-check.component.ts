ca82fbfc1bc7e1415a8e0529a01822efd1610703
Anton <antonoliverkiiski@gmail.com>
2023-01-17 13:07:26 +0200

    Create input window and prepare functionality to show inputted text

--- src/app/username-check/username-check.component.ts
+++ src/app/username-check/username-check.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-username-check',
   templateUrl: './username-check.component.html',
-  styleUrls: ['./username-check.component.css']
+  styleUrls: ['./username-check.component.css'],
 })
-export class UsernameCheckComponent {
+export class UsernameCheckComponent implements OnInit {
+  allowNewInput: boolean = false;
+  inputText = '';
+  inputCreateStatus = 'Enter text!';
 
+  constructor() {}
+
+  onCreateText() {
+    this.inputCreateStatus = this.inputText;
+  }
+
+  onUpdateInput(event: Event) {
+    this.inputText = (<HTMLInputElement>event.target).value;
+  }
+
+  ngOnInit(): void {}
 }
